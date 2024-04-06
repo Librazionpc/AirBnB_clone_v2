0477cb639a7ddd251867ea268fa63a1e0c373cc9
Librazionpc <nwajeizion393@gmail.com>
2024-04-06 01:59:02 +0100

    debugging2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,17 +11,10 @@ sudo mkdir -p /data/
 sudo chown -R "ubuntu":"ubuntu" /data/
 sudo chmod  -R 777 /data/
 
+sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p //data/web_static/releases/test/
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
-
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 nginx_config="/etc/nginx/sites-available/default"
 sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' $nginx_config
