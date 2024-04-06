a46ff97167b688f7d3102309675dd108fab7905b
Librazionpc <nwajeizion393@gmail.com>
2024-04-06 02:14:49 +0100

    Debugging5

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
-#Seetting up my server
+#set up a web server
 
-sudo apt-get update
-sudo apt-get install -y nginx
-
+source_dir="/data/web_static/releases/test/"
+symbolic_link="/data/web_static/current"
+sudo apt-get update -y
+sudo apt-get install nginx -y
 sudo mkdir -p /data/
-sudo chown -R "ubuntu":"ubuntu" /data/
-sudo chmod  -R 777 /data/
-
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p //data/web_static/releases/test/
-if [ -L "/data/web_static/current" ];
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+if [ -L "$symbolic_link" ];
 then
-	rm -d "/data/web_static/current"
+        rm -d "$symbolic_link"
 fi
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-nginx_config="/etc/nginx/sites-available/default"
-sudo sed -i "/server_name _;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n" $nginx_config
+sudo ln -s "$source_dir" "$symbolic_link"
+sudo chown -R "ubuntu":"ubuntu" /data/
+
+sudo sed -i "48i \\\tlocation /hbnb_static \{\n\t\talias \/data\/web_static\/current\/;\n\t\tautoindex off;\n\t\}" /etc/nginx/sites-available/default
+sudo nginx -t
 sudo service nginx restart
