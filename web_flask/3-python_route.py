4f6a5a3d49afa36aeb4581b503f16f7820564570
Librazionpc <nwajeizion393@gmail.com>
2024-04-20 22:13:06 +0100

    Web flask tutorial phase

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Returns Hello HBNB when the url is called"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_again():
+    """Returns HBNB when the url is called"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Returns the formated text along side the C when the url is called"""
+    underscore_to_space = text.replace('_', ' ')
+    return "C {}".format(underscore_to_space)
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text):
+    """Returns the foormatedd text along sice with python when called"""
+    underscore_to_space = text.replace('_', ' ')
+    return "Python {}".format(underscore_to_space)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ web_flask/5-number_template.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application
+"""
+from os import name
+from flask import Flask, render_template
+from numpy import number
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Returns Hello HBNB when the url is called"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_again():
+    """Returns HBNB when the url is called"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Returns the formated text along side the C when the url is called"""
+    underscore_to_space = text.replace('_', ' ')
+    return "C {}".format(underscore_to_space)
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text):
+    """Returns the foormatedd text along sice with python when called"""
+    underscore_to_space = text.replace('_', ' ')
+    return "Python {}".format(underscore_to_space)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_it_a_number(n):
+    """Return the formated integer when called"""
+    return " {} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_templates(n):
+    """Return formated html page"""
+    return render_template('5-number.html', number=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
